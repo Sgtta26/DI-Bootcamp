ee745ecb8d36415c5063fcb28fb881519afe2d0c
Sgtta26 <sarahguetta92@gmail.com>
2022-10-24 18:47:32 +0300

    dailyw3d1

---
+++ week-3/day-1/daily.js
@@ -0,0 +1,11 @@
+const planets = ["Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune"]
+
+for (const planet of planets){
+    const div = document.createElement("div")
+    div.classList.add("planet")
+    div.classList.add(planet)
+
+    const section = document.querySelector(".listPlanets")
+    section.appendChild(div)
+}
+console.log(div);
