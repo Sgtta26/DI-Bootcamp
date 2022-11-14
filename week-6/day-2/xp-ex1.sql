f569a5685a9c2d31d19fe82b300767dfa3fa21f7
Sgtta26 <sarahguetta92@gmail.com>
2022-11-14 15:12:35 +0200

    w6d2

---
+++ week-6/day-2/xp-ex1.sql
@@ -0,0 +1,11 @@
+-- select * from item;
+
+-- select price from item order by price ASC;
+
+-- select price from item order by price DESC;
+
+-- select * from custumer;
+
+-- select id, prenom, nom from custumer order by prenom ASC;
+
+select id, prenom, nom from custumer order by nom DESC;
---
+++ week-6/day-2/xp-ex2.sql
@@ -0,0 +1,28 @@
+-- select * from customer;
+
+-- select first_name as full_name, last_name as full_name from customer;
+
+-- select distinct create_date from customer;
+
+-- select * from customer order by first_name DESC;
+
+-- select film_id, title, description, release_year, rental_rate from film;
+
+-- select address, phone from address where district = 'Texas' ;
+
+-- select * from film where film_id = 15 or film_id = 150;
+
+-- select film_id, title, description, length, replacement_cost from film;
+
+-- select film_id, title, description, length, replacement_cost from film where (title like 'S%');
+
+-- select replacement_cost from film where replacement_cost=(select min(replacement_cost)from film) Limit 10;
+
+-- select replacement_cost from film where replacement_cost=(select min(replacement_cost)from film) Limit 20;
+
+-- select customer.customer_id, customer.first_name, customer.last_name as full_name, payment.amount, payment.payment_date from customer inner join payment on payment.customer_id = customer.customer_id order by customer.customer_id;
+
+select title from film;
+
+-- select country_id from city group by country_id; 
+
